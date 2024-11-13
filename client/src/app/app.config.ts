cc44c3e238dd90b376e6b9fe9c1e6c693f6e7498
Yaima Penalver Perez <YPenalver@ilshealth.com>
2024-11-12 21:05:14 -0500

    End of Section 9

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import{provideAnimations} from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { errorInterceptor } from './_interceptors/error.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 
 
 export const appConfig: ApplicationConfig = {
@@ -16,7 +17,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right'
-    })
-
+    }),
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        thumb: true,
+        counter: true
+      } as GalleryConfig
+    }
   ]
 };
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,18 +3,21 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule, NgIf],
   templateUrl: './member-detail.component.html',
-  styleUrl: './member-detail.component.css'
+  styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Member;
+  images: GalleryItem[] = [];
 
   ngOnInit(): void {
     this.loadMember();
@@ -22,10 +25,16 @@ export class MemberDetailComponent implements OnInit {
 
   loadMember(){
     const username = this.route.snapshot.paramMap.get('username');
-    if(!username) return;
+    if (!username) return;
 
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
-    })
+      next: member => {
+        this.member = member;
+        member.photos?.forEach(p => {
+          const thumbUrl = p.url.replace('.jpg', '-thumb.jpg'); // Assuming you have thumbnail versions
+          this.images.push(new ImageItem({ src: p.url, thumb: thumbUrl }));
+        });
+      }
+    });
   }
 }
