b34b0d263e81e1ff4ba8206703003335f63bcd7d
Yaima Penalver Perez <YPenalver@ilshealth.com>
2024-11-22 12:46:28 -0500

    End of Section 14

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { LikesService } from '../_services/likes.service';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { FormsModule } from '@angular/forms';
@@ -12,8 +12,7 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent implements OnInit {
- 
+export class ListsComponent implements OnInit, OnDestroy {
   likesService = inject(LikesService);
   predicate = 'liked';
   pageNumber = 1;
@@ -42,5 +41,8 @@ export class ListsComponent implements OnInit {
     }
   }
 
+  ngOnDestroy(): void {
+    this.likesService.paginatedResults.set(null);
+  }
 
 }
